e4a48decbc48dc9763ab40fbc8d72be3a36c5877
wentuoc <e1113779@u.nus.edu>
2025-01-31 13:56:23 +0800

    Add methods to echo text and exit loop

--- src/main/java/Camel.java
+++ src/main/java/Camel.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Camel {
+    public static void echoInput(String input) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Camel says: \"" + input + "\" back :)");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void exitLoop() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Camel says: Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Camel");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Camel");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while (true) {
+            input = in.nextLine();
+            if (!input.equals("bye")) {
+                echoInput(input);
+            } else {
+                exitLoop();
+                break;
+            }
+        }
     }
 }
