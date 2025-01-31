2c95e9fba054e844a8f3fac743dbbb08844a26ec
wentuoc <e1113779@u.nus.edu>
2025-01-31 14:29:36 +0800

    Add methods to store and retrieve tasks

--- src/main/java/Camel.java
+++ src/main/java/Camel.java
@@ -1,32 +1,55 @@
 import java.util.Scanner;
 
 public class Camel {
-    public static void echoInput(String input) {
+    private static int numberOfTasks = 0;
+    private static String[] tasks = new String[100];
+
+    private static void addToList(String input) {
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Camel says: \"" + input + "\" back :)");
+        System.out.println("    Camel has added: " + input + " :)");
+        System.out.println("    ____________________________________________________________");
+        tasks[numberOfTasks] = input;
+        numberOfTasks++;
+    }
+
+    private static void printList() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Of course! Camel shall gladly retrieve your tasks :)");
+        for (int i = 0; i < numberOfTasks; i++) {
+            System.out.printf("    %d. %s%n", i+1, tasks[i]);
+        }
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void exitLoop() {
+    private static void exitLoop() {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Camel says: Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input;
+        boolean hasEnded = false;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Camel");
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________________________");
 
-        while (true) {
+        while (!hasEnded) {
             input = in.nextLine();
-            if (!input.equals("bye")) {
-                echoInput(input);
-            } else {
+            String[] words = input.split(" ");
+            switch (words[0]) {
+            case "list":
+                printList();
+                break;
+            case "bye":
                 exitLoop();
+                hasEnded = true;
+                break;
+            default:
+                addToList(input);
                 break;
             }
         }
