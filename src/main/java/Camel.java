7ce0af1837f79ee10686c5ed044b802385a2a75c
wentuoc <e1113779@u.nus.edu>
2025-01-31 15:20:11 +0800

    Modify Camel.java to use Task class
    Camel stores tasks and their associated attributes in multiple arrays
    within the class.
    
    It is better to think of tasks as a separate object and hence a
    separate class.
    
    Let's modify the methods within Camel to create and modify new Task
    instances instead.

--- src/main/java/Camel.java
+++ src/main/java/Camel.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Camel {
     private static int numberOfTasks = 0;
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
 
-    private static void addToList(String input) {
+    private static void addTask(String input) {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Camel has added: " + input + " :)");
         System.out.println("    ____________________________________________________________");
-        tasks[numberOfTasks] = input;
+        Task newTask = new Task(input);
+        tasks[numberOfTasks] = newTask;
         numberOfTasks++;
     }
 
@@ -16,14 +17,42 @@ public class Camel {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Of course! Camel shall gladly retrieve your tasks :)");
         for (int i = 0; i < numberOfTasks; i++) {
-            System.out.printf("    %d. %s%n", i+1, tasks[i]);
+            Task currentTask = tasks[i];
+            System.out.printf("    %d.", i + 1);
+            currentTask.printTask();
         }
         System.out.println("    ____________________________________________________________");
     }
 
+    private static void markTask(String input) {
+        int index = Integer.parseInt(input);
+        if (index >= 1 && index <= numberOfTasks) {
+            Task currentTask = tasks[index-1];
+            currentTask.setDone();
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    Nice! Camel has marked this task as done :)");
+            System.out.print("    ");
+            currentTask.printTask();
+            System.out.println("    ____________________________________________________________");
+        }
+    }
+
+    private static void unmarkTask(String input) {
+        int index = Integer.parseInt(input);
+        if (index >= 1 && index <= numberOfTasks) {
+            Task currentTask = tasks[index-1];
+            currentTask.setNotDone();
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    Ok, Camel has marked this task as not done yet :)");
+            System.out.print("    ");
+            currentTask.printTask();
+            System.out.println("    ____________________________________________________________");
+        }
+    }
+
     private static void exitLoop() {
         System.out.println("    ____________________________________________________________");
-        System.out.println("    Camel says: Bye. Hope to see you again soon!");
+        System.out.println("    Camel says bye! Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 
@@ -44,12 +73,18 @@ public class Camel {
             case "list":
                 printList();
                 break;
+            case "mark":
+                markTask(words[1]);
+                break;
+            case "unmark":
+                unmarkTask(words[1]);
+                break;
             case "bye":
                 exitLoop();
                 hasEnded = true;
                 break;
             default:
-                addToList(input);
+                addTask(input);
                 break;
             }
         }
